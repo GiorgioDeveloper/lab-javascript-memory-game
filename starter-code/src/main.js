2c9a3f9928eab1b897569d995f91f4b77fff304c
Giorgio <Giorgio@Clementines-MacBook-Pro.local>
2019-08-12 19:35:08 +0200

    Final Update

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,27 +25,84 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 var memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards()
 
+document.addEventListener("DOMContentLoaded", function main (event) { 
+ 
+  function render(){
+    var html = '';
+    memoryGame.cards.forEach(function (pic) {
+      html += '<div class="card" data-card-name="'+ pic.name +'">';
+      html += '  <div class="back" name="'+ pic.img +'"></div>';
+      html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
+      html += '</div>';
+    });
+  
+    // Add all the div's to the HTML
+    document.querySelector('#memory_board').innerHTML = html;
+  }
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var html = '';
-  memoryGame.cards.forEach(function (pic) {
-    html += '<div class="card" data-card-name="'+ pic.name +'">';
-    html += '  <div class="back" name="'+ pic.img +'"></div>';
-    html += '  <div class="front" style="background: url(img/'+ pic.img +') no-repeat"></div>';
-    html += '</div>';
-  });
-
-  // Add all the div's to the HTML
-  document.querySelector('#memory_board').innerHTML = html;
-
+  render();
   // Bind the click event of each element to a function
-  document.querySelectorAll('.back').forEach(function(card) {
-    card.onclick = function() {
-      // TODO: write some code here
-      console.log('Card clicked')
-    }
-  });
-});
 
+  // $(".back").click(function(){
+  //   $(this).attr("class", "front").siblings().attr("class", "back");
+  // });
+
+
+
+  document.querySelectorAll('.back').forEach(function(card) {
+    card.onclick = function () {
+      $(this).attr("class", "front").siblings().attr("class", "back");
+      memoryGame.pickedCards.push(this);
+      
+      
+      if(memoryGame.pickedCards.length == 2){
+        if(!memoryGame.checkIfPair()) {
+          const card1 = memoryGame.pickedCards[0]
+          const card2 = memoryGame.pickedCards[1]
+          
+          $(".back").css("pointer-events", "none");
+
+          setTimeout(function(){
+            $(card1).attr("class", "back").siblings().attr("class", "front");
+            $(card2).attr("class", "back").siblings().attr("class", "front");
+            $(".back").css("pointer-events", "auto");
+          },1000);
+
+          
+
+        }
+        memoryGame.pickedCards = [];
+      
+        if(memoryGame.isFinished()){
+          setTimeout(function(){
+
+      $(".winHide").attr("class", "win");
+      $(".playAgainHide").attr("class", "playAgain").click(function(){
+
+        $(".win").attr("class", "winHide");
+        $(".playAgain").attr("class", "playAgainHide");
+         memoryGame.pairsClicked = 0;
+        $("#pairs_clicked").html(memoryGame.pairsClicked);
+         memoryGame.pairsGuessed = 0;
+        $("#pairs_guessed").html(memoryGame.pairsGuessed);
+        
+        memoryGame.shuffleCards()
+        render();
+        main();
+        
+        
+
+      });
+
+
+           },500);
+          }
+      }
+   }
+
+   
+ });
+});
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,8 +1,50 @@
 class MemoryGame {
   constructor(card){
     this.cards = cards;
+    this.pickedCards =[];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
-  isFinished() {}
+  shuffleCards() {
+    
+   
+
+    for (let i = 0; i < this.cards.length; i++) {
+      let j = Math.floor(Math.random() * i );
+      const element = this.cards[i];
+      if( j  !== i ) {
+        this.cards[i] = this.cards[j];
+      }
+      this.cards[j] = element;
+    }
+    
+  }
+
+  
+
+  checkIfPair() {
+    const card1 = this.pickedCards[0].outerHTML
+    const card2 = this.pickedCards[1].outerHTML;
+
+   
+    this.pairsClicked++
+    $("#pairs_clicked").html(this.pairsClicked);
+    if (card1 == card2) {
+      this.pairsGuessed++ 
+      $("#pairs_guessed").html(this.pairsGuessed);
+      return true
+    } else {
+      return false
+    }
+  }
+
+  isFinished() {
+    if(this.pairsGuessed == 12){
+return true
+    } else {
+      return false
+    }
+  }
 }
+
